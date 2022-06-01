5647829171555e5d10173c7f6627091b6efe398f
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2022-06-01 20:30:02 -0300

    Projeto em andamento <3

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+const horas = document.getElementById('hours')
+const minutos = document.getElementById('minutes')
+const segundos = document.getElementById('seconds')
+
+const relogio = setInterval(function time() {
+  let dateToday = new Date()
+  let hr = dateToday.getHours()
+  let min = dateToday.getMinutes()
+  let s = dateToday.getSeconds()
+
+  if (hr < 10) hr = '0' + hr
+  if (min < 10) min = '0' + min
+  if (s < 10) s = '0' + s
+
+  horas.textContent = hr
+  minutos.textContent = min
+  segundos.textContent = s
+})
